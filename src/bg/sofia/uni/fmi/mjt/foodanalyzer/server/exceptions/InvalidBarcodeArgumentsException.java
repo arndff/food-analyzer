6425b2af7fe0407fbec95204bac917dccdb66678
arndff_ <jrarnaudov@gmail.com>
2019-03-19 19:26:14 +0200

    Rename a constant in two of the exception classes

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/InvalidBarcodeArgumentsException.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/InvalidBarcodeArgumentsException.java
@@ -1,9 +1,9 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions;
 
 public class InvalidBarcodeArgumentsException extends Exception {
-    private static final String EXCEPTION_MSG = "Get food by barcode was called with invalid argument(s).";
+    private static final String EXCEPTION_MESSAGE = "Get food by barcode was called with invalid argument(s).";
 
     public InvalidBarcodeArgumentsException() {
-        super(EXCEPTION_MSG);
+        super(EXCEPTION_MESSAGE);
     }
 }
--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/InvalidQueryTypeException.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/exceptions/InvalidQueryTypeException.java
@@ -1,9 +1,9 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.server.exceptions;
 
 public class InvalidQueryTypeException extends Exception {
-    private static final String EXCEPTION_MSG = "Invalid query type.";
+    private static final String EXCEPTION_MESSAGE = "Invalid query type.";
 
     public InvalidQueryTypeException() {
-        super(EXCEPTION_MSG);
+        super(EXCEPTION_MESSAGE);
     }
 }
