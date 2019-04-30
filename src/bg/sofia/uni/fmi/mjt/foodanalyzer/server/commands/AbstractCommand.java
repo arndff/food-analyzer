74ad3593f15d8799d168f9363911979c7c33ea39
arndff_ <jrarnaudov@gmail.com>
2019-04-30 22:33:10 +0300

    Refactor

--- src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/commands/AbstractCommand.java
+++ src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/commands/AbstractCommand.java
@@ -8,16 +8,16 @@ import java.net.http.HttpClient;
 import java.util.logging.Logger;
 
 public abstract class AbstractCommand {
+    static final String API_URL = "https://api.nal.usda.gov/ndb";
+    static final String API_KEY = "yqVQElHgqao3jzD9KbtKeygI2UqpOf41XYbNpcd9";
+
+    static final Logger foodServerLogger = FoodServer.getFoodServerLogger();
+
     HttpClient client;
 
     AbstractCommand(HttpClient client) {
         this.client = client;
     }
-
-    static final String API_URL = "https://api.nal.usda.gov/ndb";
-    static final String API_KEY = "yqVQElHgqao3jzD9KbtKeygI2UqpOf41XYbNpcd9";
-
-    static final Logger foodServerLogger = FoodServer.getFoodServerLogger();
 
     public abstract String execute(String argument) throws InvalidBarcodeArgumentsException, NoInformationFoundException;
 }
